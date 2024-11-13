072b462268517599d165f4c6f7581f6309bbb405
MARCO ANTONIO SANTOS MEDEIROS <m.medeiros@labcetec.net>
2024-11-13 16:13:34 -0300

    adicionando login e logout

--- client/src/views/GestaoImagem.jsx
+++ client/src/views/GestaoImagem.jsx
@@ -1,13 +1,48 @@
 import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 function GestaoImagem() {
     const [imagens, setImagens] = useState([]);
     const [imagem, setImagem] = useState(null);
     const [descricao, setDescricao] = useState('');
+    const [idUsuario, setIdusuario] = useState('');
+    const navigate = useNavigate();
+    const [login, setLogin] = useState('');
+    const [funcao, setFuncao] = useState('');
 
     useEffect(() => {
         carregarImagens()
+        if (idUsuario === '') {
+            try {
+                const id_usuario = localStorage.getItem('id_usuario');
+                if (!id_usuario) {
+                    alert('Efetue Login')
+                    navigate('/login');
+                } else {
+                    setIdusuario(id_usuario);
+                    getNomeFuncao(id_usuario);
+                }
+            } catch (error) {
+                console.log('')
+            }
+        }
+        carregarImagens();
     }, []);
+
+    async function getNomeFuncao(id_usuario) {
+        console.log(`http://localhost:5000/usuario/${id_usuario}`);
+        try {
+            const resposta = await fetch(`http://localhost:5000/usuario/${id_usuario}`);
+            const dados = await resposta.json();
+            if (dados) {
+                console.log(dados);
+                setLogin(dados.login);
+                setFuncao(dados.funcao);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
     async function cadastrarImagem() {
         const formData = new FormData();
@@ -68,6 +103,11 @@ function GestaoImagem() {
         }
     }
 
+    function logout() {
+        localStorage.removeItem('id_usuario');
+        navigate('/login');
+    }
+
     return (
         <>
             <div>
@@ -76,10 +116,13 @@ function GestaoImagem() {
                     <ul>
                         <li>Inicio</li>
                     </ul>
+                    <button className='btn btn-danger' onClick={logout}>Logout</button>
                 </nav>
             </div>
             <div className='container'>
                 <h1 className='text-center'>Gestão Imagens</h1>
+                <h2>{`Bem vindo ${login}`}</h2>
+                <h3>{funcao === 'adm' && 'voce e administrador!!!'}</h3>
                 <div>
                     <h2>Cadastrar imagem</h2>
                     <label htmlFor="">Descrição</label>
